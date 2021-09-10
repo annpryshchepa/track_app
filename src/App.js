d04c2a0bf5e37b1a5e32036a58a502723af34f70
Hanna Pryshchepa <hpryshchepa@griddynamics/com>
2021-09-10 16:55:19 +0300

    added fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React, { useState } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
+import { Dashboard, HomePage } from './components';
+import classnames from 'classnames';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+  
 
+/* eslint-disable no-lone-blocks */
 // const click = () => console.log('click');
 //TODO use these buttons 
 {/* <Button
@@ -31,14 +34,42 @@ buttonIcon="stop"
 
 
 export const App = () => {
+  const home = 'home';
+  const dashboard = 'dashboard';
+  const [activeTab, setActiveTab] = useState(home);
+
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === home })}
+            onClick={() => { toggle(home); }}
+          >
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === dashboard })}
+            onClick={() => { toggle(dashboard); }}
+          >
+            Dashboard
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId={home}>
+              <HomePage />
+        </TabPane>
+        <TabPane tabId={dashboard}>
+              <Dashboard />
+        </TabPane>
+      </TabContent>
     </div>
   );
 }
