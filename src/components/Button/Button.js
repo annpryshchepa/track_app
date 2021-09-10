ac0cdf8fd1ac8ed6fe9d8950fd4bc1a8d697f1e7
Hanna Pryshchepa <hpryshchepa@griddynamics/com>
2021-09-10 15:17:22 +0300

    added styled buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,27 @@
+import { Button } from 'reactstrap';
+import PropTypes from "prop-types";
+import "../../icon/stopwatch.svg";
+
 import './Button.scss';
 
-export const Button = () => {
+export const CustomButton = ({ content, onClickAction, isOutline, buttonIcon, className }) => {
   return (
-    <div className="App">
-      <p>this is button</p>
-    </div>
+    <Button outline={isOutline} onClick={onClickAction()} className={className}>
+      <span className={buttonIcon}></span>
+      {content}
+    </Button>
   );
+};
+
+CustomButton.propTypes = {
+  content: PropTypes.string.isRequired,
+  onClickAction: PropTypes.func,
+  isOutline: PropTypes.bool,
+  className: PropTypes.string
+};
+
+CustomButton.defaultProps = {
+  onClickAction: () => {},
+  isOutline: false,
+  className: ""
 };
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,1 +1,1 @@
-export { Button } from './Button'; 
+export { CustomButton as Button } from './Button'; 
